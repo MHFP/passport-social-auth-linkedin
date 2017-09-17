fa2ea29cebea3e9b08e7a4b85a386a4a0449fa29
MHFP <markus.hifp@gmail.com>
2017-09-17 15:18:54 +0200

    require linkedinPassport and update route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passportLinkedIn = require('../auth/linkedin');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,5 +9,14 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.send('Go back and register!');
 });
+
+router.get('/auth/linkedin', passportLinkedIn.authenticate('linkedin'));
+
+router.get('/auth/linkedin/callback',
+  passportLinkedIn.authenticate('linkedin', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication
+    res.json(req.user);
+  });
 
 module.exports = router;
